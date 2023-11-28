c234ac40189c894df8ae46642f11642e41480fbb
tobi adeogun <tobiadeomax@outlook.com>
2023-11-28 14:20:37 +0100

    using Entity framework

--- EFCore_Activity0201/Program.cs
+++ EFCore_Activity0201/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+class Program
+{
+    private static IConfigurationRoot _configuration;
+    private static DbContextOptionsBuilder<AdventureWorksContext> _optionBuilder;
+    static void Main(string[] args)
+    {
+        BuildConfiguration();
+        BuilOptions();
+        ListPeople();
+    }
+
+    static void BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        _configuration = builder.Build();
+
+        Console.WriteLine($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
+    }
+
+    static void BuilOptions()
+    {
+        _optionBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+        _optionBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+    }
+
+    static void ListPeople()
+    {
+        using(var db = new AdventureWorksContext(_optionBuilder.Options))
+        {
+            var people = db.People.OrderByDescending(x => x.LastName).Take(20).ToList();
+
+
+            foreach(var person in people)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}");
+            }
+        }
+    }
+}
+
+ 
+
+
+
